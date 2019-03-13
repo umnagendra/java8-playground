bb2b168a315d7f6c3ac867baea0979bc72b9e4d6
Nagendra Mahesh <namahesh@cisco.com>
2019-03-13 12:46:10 +0530

    Maven project structure

---
+++ src/test/java/functional/Lambdas.java
@@ -0,0 +1,13 @@
+package functional;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Lambdas {
+
+    @Test
+    public void testFoo() {
+        assertTrue(true);
+    }
+}
